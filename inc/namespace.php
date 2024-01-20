18854fa7ab63f33445ba5e1983545b2907251ab6
pdewouters <paul@dekode.no>
2024-01-20 16:53:33 +0000

    Load files

---
+++ inc/namespace.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * TNB_Top_News namespace.
+ *
+ * @package TNB_Top_News
+ **/
+
+declare( strict_types=1 );
+
+namespace TNB_Top_News;
+
+/**
+ * Init modules.
+ *
+ * @return void
+ */
+function setup(): void {
+	Blocks\setup();
+	REST\setup();
+}
