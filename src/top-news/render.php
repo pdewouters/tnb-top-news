34a14034d246b48362db4a2d9948094e30313ccd
pdewouters <paul@dekode.no>
2024-01-20 17:25:45 +0000

    Render innerblocks

--- src/top-news/render.php
+++ src/top-news/render.php
@@ -1,8 +1,3 @@
-<?php
-/**
- * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
- */
-?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Tnb Top News – hello from a dynamic block!', 'tnb-top-news' ); ?>
-</p>
+<div <?php echo get_block_wrapper_attributes(); ?>>
+	<?php echo $content; ?>
+</div>
