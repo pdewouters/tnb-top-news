9e29118eaf290c7aa93fa85f00a4269af9ddc703
pdewouters <paul@dekode.no>
2024-01-20 17:26:02 +0000

    Temporary frontend markup

--- src/country-top-news/render.php
+++ src/country-top-news/render.php
@@ -1,6 +1,19 @@
 <?php
 $country_code = $attributes['countryCode'];
 $articles     = TNB_Top_News\API\get_articles( $country_code );
-var_dump($articles);
 ?>
-
+<div <?php echo get_block_wrapper_attributes(); ?>>
+	<?php if ( empty( $articles ) ) : ?>
+		<p><?php esc_html_e( 'No articles found.', 'tnb-top-news' ); ?></p>
+	<?php else : ?>
+		<ul>
+			<?php foreach ( $articles as $article ) : ?>
+				<li>
+					<a href="<?php echo esc_url( $article['url'] ); ?>">
+						<?php echo esc_html( $article['title'] ); ?>
+					</a>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	<?php endif; ?>
+</div>
