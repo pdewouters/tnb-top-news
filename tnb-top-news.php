b45b1fd5485a254354e43da467e7743f8a8ad961
pdewouters <paul@dekode.no>
2024-01-21 10:32:01 +0000

    Include utils

--- tnb-top-news.php
+++ tnb-top-news.php
@@ -9,8 +9,9 @@
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       tnb-top-news
+ * Domain Path:       /languages
  *
- * @package           create-block
+ * @package           TNB_Top_News
  */
 
 declare(strict_types=1);
@@ -19,6 +20,7 @@ namespace TNB_Top_News;
 
 const DIR = __DIR__;
 
+require_once __DIR__ . '/inc/utils.php';
 require_once __DIR__ . '/inc/blocks.php';
 require_once __DIR__ . '/inc/rest.php';
 require_once __DIR__ . '/inc/api.php';
