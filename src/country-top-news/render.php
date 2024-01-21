4b19391c46c52531b4470afdecce11299d1b1c23
pdewouters <paul@dekode.no>
2024-01-21 18:46:12 +0000

    codesniffer config

--- src/country-top-news/render.php
+++ src/country-top-news/render.php
@@ -12,13 +12,17 @@ namespace TNB_Top_News\Country_Top_News;
 use TNB_Top_News\API;
 use TNB_Top_News\Utils;
 
-$country_code = $attributes['countryCode'];
+$tnb_country_code   = $attributes['countryCode'];
+$tnb_articles       = API\get_articles( $tnb_country_code );
+$tnb_country_name   = Utils\get_country_name( $tnb_country_code );
+$tnb_handle         = \generate_block_asset_handle( 'tnb/top-news', 'viewScript' );
+$tnb_inline_script  = '';
+$tnb_inline_script .= 'if (typeof tnbTopNewsAppData === "undefined") { var tnbTopNewsAppData = {}; }' . PHP_EOL;
+$tnb_inline_script .= 'tnbTopNewsAppData.' . $tnb_country_code . ' = ' . \wp_json_encode(
+	[
+		'articles'    => $tnb_articles,
+		'countryName' => $tnb_country_name,
+	]
+) . ';' . PHP_EOL;
 
-$articles       = API\get_articles( $country_code );
-$country_name   = Utils\get_country_name( $country_code );
-$handle         = \generate_block_asset_handle( 'tnb/top-news', 'viewScript' );
-$inline_script  = '';
-$inline_script .= 'if (typeof tnbTopNewsAppData === "undefined") { var tnbTopNewsAppData = {}; }' . PHP_EOL;
-$inline_script .= 'tnbTopNewsAppData.' . $country_code . ' = ' . \wp_json_encode( ['articles' => $articles, 'countryName' => $country_name ] ) . ';' . PHP_EOL;
-
-\wp_add_inline_script( $handle, $inline_script, 'before' );
+\wp_add_inline_script( $tnb_handle, $tnb_inline_script, 'before' );
