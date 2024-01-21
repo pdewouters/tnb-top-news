0a1e1121648e0ab7bad6e8a778d2ac528f239a60
pdewouters <paul@dekode.no>
2024-01-21 13:56:18 +0000

    Render country news as tabs

--- src/top-news/components/App.js
+++ src/top-news/components/App.js
@@ -1,12 +1,42 @@
-//import "./style.css";
-import * as Ariakit from "@ariakit/react";
-import TabList from './TabList';
-import TabPanel from './TabPanel';
-export default function App({tabsData}) {
+import * as Ariakit from '@ariakit/react';
 
+import Article from './Article';
+
+export default function App( { tabsData } ) {
+	const defaultSelectedId = 'default-selected-tab';
 	return (
-		<div className="wrapper">
-			{JSON.stringify(tabsData)}
-		</div>
+		<>
+			<Ariakit.TabProvider defaultSelectedId={ defaultSelectedId }>
+				<Ariakit.TabList className="tab-list" aria-label="Groceries">
+					{ Object.keys( tabsData ).map( ( tab, index ) => (
+						<Ariakit.Tab key={ tab } id={ index === 0 ? defaultSelectedId : tab } className="tab">
+							{ tabsData[ tab ].countryName }
+						</Ariakit.Tab>
+					) ) }
+				</Ariakit.TabList>
+				<div className="panels">
+					{ Object.keys( tabsData ).map( ( tab, index ) => (
+						<Ariakit.TabPanel
+							key={ tab }
+							tabId={ index === 0 ? defaultSelectedId : tab }
+						>
+							<h3>
+								Headlines for { tabsData[ tab ].countryName }
+							</h3>
+							<ul>
+								{ tabsData[ tab ].articles.map(
+									( article, index ) => (
+										<Article
+											key={ `article-${ tabsData[ tab ].countryName }-${ index }` }
+											articleData={ article }
+										/>
+									)
+								) }
+							</ul>
+						</Ariakit.TabPanel>
+					) ) }
+				</div>
+			</Ariakit.TabProvider>
+		</>
 	);
 }
---
+++ src/top-news/components/Article.js
@@ -0,0 +1,9 @@
+export default function Article({articleData}) {
+  return (
+	  <li>
+		  <article>
+			  <h4>{articleData.title}</h4>
+		  </article>
+	  </li>
+  );
+}
