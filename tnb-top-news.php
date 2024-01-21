3f8ed59ccc865c6fd9606c574278a05cbe512b1b
pdewouters <paul@dekode.no>
2024-01-21 19:50:27 +0000

    Fix docs

--- tnb-top-news.php
+++ tnb-top-news.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Tnb Top News
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       Displays the top news articles from the NEWS API.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Paul de Wouters
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       tnb-top-news
