a6f9166548c3f069512ea5bb6373d04f6ed4f6c9
pdewouters <paul@dekode.no>
2024-01-21 10:27:36 +0000

    Add a title to the country section

--- src/country-top-news/render.php
+++ src/country-top-news/render.php
@@ -1,10 +1,35 @@
 <?php
-$country_code = $attributes['countryCode'];
-$articles     = TNB_Top_News\API\get_articles( $country_code );
+/**
+ * Top News block render callback.
+ *
+ * @package TNB_Top_News
+ */
+
+declare(strict_types=1);
+
+namespace TNB_Top_News\Country_Top_News;
+
+use TNB_Top_News\API;
+use TNB_Top_News\Utils;
+
+$country_code = /** @var string $attributes */ $attributes['countryCode'];
+$articles     = API\get_articles( $country_code );
+$country_name = Utils\get_country_name( $country_code );
 ?>
-<div <?php echo get_block_wrapper_attributes(); ?>>
+<div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
+	<h3>
+		<?php
+		printf(
+			/* translators: %s: Country name. */
+			esc_html__( 'Top News from %s', 'tnb-top-news' ),
+			esc_html( $country_name )
+		);
+		?>
+	</h3>
 	<?php if ( empty( $articles ) ) : ?>
-		<p><?php esc_html_e( 'No articles found.', 'tnb-top-news' ); ?></p>
+		<p>
+			<?php esc_html_e( 'No articles found.', 'tnb-top-news' ); ?>
+		</p>
 	<?php else : ?>
 		<ul>
 			<?php foreach ( $articles as $article ) : ?>
